e069abdc4ce31d1324da4dd71e2230960a070ed5
devgony <devgony@gmail.com>
2021-06-11 16:09:20 +0900

    codegen url

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,10 @@ module.exports = {
     tagName: "gql",
     service: {
       name: "instaclone-backend",
-      url: "http://localhost:4001/graphql",
+      url:
+        process.env.NODE_ENV === "production"
+          ? "https://instaclone-backend-henry.herokuapp.com/graphql"
+          : "http://localhost:4001/graphql",
     },
   },
 };
